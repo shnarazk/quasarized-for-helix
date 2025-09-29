dc368da22e88668e2970b9a03c4e6acf39f7e94b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-09-29 12:36:08 +0900

    modified:   devsh.fish

--- devsh.fish
+++ devsh.fish
@@ -1,4 +1,9 @@
 function quasarized-dev
-    ln -sf $PWD/quasarized_dark.toml $HOME/.config/helix/themes
-    ln -sf $PWD/quasarized_light.toml $HOME/.config/helix/themes
+    set target_dir $HOME/.config/helix/themes
+    if not test -d $target_dir
+        mkdir -p $target_dir
+    end
+    ln -sf $PWD/quasarized_dark.toml $target_dir/quasarized_dark.toml
+    ln -sf $PWD/quasarized_light.toml $target_dir/quasarized_light.toml
+    ls -l $dir
 end
